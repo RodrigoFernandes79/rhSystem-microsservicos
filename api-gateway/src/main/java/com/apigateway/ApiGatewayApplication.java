f59a3c2ee65538dc545abfa7defd38dff4819bf1
Rodrigo Fernandes <83513696+RodrigoFernandes79@users.noreply.github.com>
2022-07-02 15:20:52 -0300

    API Gateway created

--- api-gateway/src/main/java/com/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/apigateway/ApiGatewayApplication.java
@@ -1,17 +1,15 @@
-package com.payrollapi;
+package com.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 @EnableEurekaClient
-public class PayrollApiApplication {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PayrollApiApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
